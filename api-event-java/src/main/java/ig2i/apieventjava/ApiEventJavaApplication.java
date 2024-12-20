842cc957dc2c0a0951e79bcbd12025cfd94d951f
patacoing <romain.lefebvre1104@gmail.com>
2024-12-20 10:02:08 +0100

    initial commit: api-event-java

---
+++ api-event-java/src/main/java/ig2i/apieventjava/ApiEventJavaApplication.java
@@ -0,0 +1,13 @@
+package ig2i.apieventjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiEventJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiEventJavaApplication.class, args);
+	}
+
+}
---
+++ api-event-java/src/test/java/ig2i/apieventjava/ApiEventJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package ig2i.apieventjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiEventJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
