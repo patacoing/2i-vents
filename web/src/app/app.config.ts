81b058f88e2196728c353c1e65097fbf55af9ce9
Marius-courquin <marius.courquin@ig2i.centralelille.fr>
2024-12-20 12:16:35 +0100

    fix(web): primeng config

--- web/src/app/app.config.ts
+++ web/src/app/app.config.ts
@@ -1,8 +1,25 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import {ApplicationConfig, provideZoneChangeDetection} from '@angular/core';
+import {provideRouter} from '@angular/router';
 
-import { routes } from './app.routes';
+import {routes} from './app.routes';
+import {providePrimeNG} from 'primeng/config';
+import {provideAnimationsAsync} from '@angular/platform-browser/animations/async';
+import Material from '@primeng/themes/material';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({eventCoalescing: true}),
+    provideRouter(routes),
+    providePrimeNG({
+      theme: {
+        preset: Material,
+        options: {
+          prefix: 'p',
+          darkModeSelector: 'system',
+          cssLayer: false
+        }
+      }
+    }),
+    provideAnimationsAsync()
+  ]
 };
--- web/src/app/pages/user/user.component.ts
+++ web/src/app/pages/user/user.component.ts
@@ -1,8 +1,16 @@
 import { Component } from '@angular/core';
+import {Tab, TabList, TabPanel, TabPanels, Tabs, TabsModule} from 'primeng/tabs';
 
 @Component({
   selector: 'app-user',
-  imports: [],
+  imports: [
+    TabsModule,
+    TabPanel,
+    TabPanels,
+    TabList,
+    Tabs,
+    Tab
+  ],
   templateUrl: './user.component.html',
   styleUrl: './user.component.scss'
 })
