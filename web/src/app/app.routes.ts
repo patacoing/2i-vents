a072da91cb5ba073a0cd3ecd8e7729dae5da9707
Marius-courquin <marius.courquin@ig2i.centralelille.fr>
2024-12-20 11:57:52 +0100

    feat(web): setup routing

--- web/src/app/app.routes.ts
+++ web/src/app/app.routes.ts
@@ -1,3 +1,23 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
+import {UserComponent} from './pages/user/user.component';
+import {LoginComponent} from './pages/user/login/login.component';
+import {RegisterComponent} from './pages/user/register/register.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'user',
+    component: UserComponent,
+    children: [
+      {
+        path: 'login',
+        component: LoginComponent,
+      },
+      {
+        path: 'register',
+        component: RegisterComponent,
+      },
+    ],
+  },
+  { path: '',   redirectTo: '/user/login', pathMatch: 'full' }
+];
+
